5cd8a29c0e57b54c33a9e65720658d2abf7e9d13
MelathStofi <melath.stofi@gmail.com>
2020-04-20 14:49:55 +0200

    create eureka server

---
+++ src/test/java/com/codecool/quizcodecool/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.quizcodecool.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
